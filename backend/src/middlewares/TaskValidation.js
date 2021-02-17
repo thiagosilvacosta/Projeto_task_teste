3d56b0ccf2ff85590689d7603a1227764a32e157
thiagosilvacosta <67482943+thiagosilvacosta@users.noreply.github.com>
2021-02-16 22:43:35 -0300

    finalizando validaçõs

--- backend/src/middlewares/TaskValidation.js
+++ backend/src/middlewares/TaskValidation.js
@@ -16,8 +16,19 @@ const TaskValidation = async (req, res, next) => {
         return res.status(400).json({error: 'Data e Hora é Obrigatório!'})
     else if(isPast(new Date(when)))
         return res.status(400).json({error: 'Escolha uma data e Hora Futura!'})
-    else
+        else{
+            let exists;
+            
+            exists = await TaskModel.findOne({
+                                        'when': {'$eq': new Date(when)},
+                                        'macaddress' : {'$in': macaddress }
+                                        })
+            if(exists){
+                return res.status(400).json({error: 'Já existe uma Tarefa neste dia e Horário para seu Dispositivo!'})
+            }
+        }
         next()
+
 }
 
 module.exports = TaskValidation;
