bb1e007f0d05f516841c1c0487f56f85b697ed03
thiagosilvacosta <67482943+thiagosilvacosta@users.noreply.github.com>
2021-02-16 21:51:56 -0300

    configurando conexão com o banco de dados e criando tabela

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const server = express()
 
+server.get('/teste', (req,res)=>{
+    res.send('Tudo certo com nossa API')
+})
+
 server.listen(3000, ()=>{
     console.log('Executando na porta 3000')
 })
