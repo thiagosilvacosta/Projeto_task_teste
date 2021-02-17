07358daedf8b15caffffc916cc777599e4a61a2b
thiagosilvacosta <67482943+thiagosilvacosta@users.noreply.github.com>
2021-02-16 22:03:49 -0300

    criando rotas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
+server.use(express.json());
 
-server.get('/teste', (req,res)=>{
-    res.send('Tudo certo com nossa API')
-})
+const TaskRoutes = require('./routes/TaskRoutes');
+
+server.use('/task', TaskRoutes);
+
 
 server.listen(3000, ()=>{
-    console.log('Executando na porta 3000')
+    console.log('Executando na porta 3000');
 })
