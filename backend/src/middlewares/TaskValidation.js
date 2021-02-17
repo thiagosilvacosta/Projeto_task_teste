f764e4279734bc97744594dba01b46712b6d136c
thiagosilvacosta <67482943+thiagosilvacosta@users.noreply.github.com>
2021-02-16 23:02:14 -0300

    finalizando validação de update

--- backend/src/middlewares/TaskValidation.js
+++ backend/src/middlewares/TaskValidation.js
@@ -17,12 +17,24 @@ const TaskValidation = async (req, res, next) => {
     else if(isPast(new Date(when)))
         return res.status(400).json({error: 'Escolha uma data e Hora Futura!'})
         else{
+
             let exists;
             
-            exists = await TaskModel.findOne({
-                                        'when': {'$eq': new Date(when)},
-                                        'macaddress' : {'$in': macaddress }
-                                        })
+            if(req.params.id){
+                exists = await TaskModel.findOne(
+                    
+                    {   '_id': {'$ne': req.params.id},
+                        'when': {'$eq': new Date(when)},
+                        'macaddress' : {'$in': macaddress }
+                    })
+            } else{
+
+                exists = await TaskModel.findOne(
+                                            {'when': {'$eq': new Date(when)},
+                                            'macaddress' : {'$in': macaddress }
+                                            })
+            }
+
             if(exists){
                 return res.status(400).json({error: 'Já existe uma Tarefa neste dia e Horário para seu Dispositivo!'})
             }
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -5,6 +5,7 @@ const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation')
 
 router.post('/',TaskValidation,TaskController.create);
+router.put('/:id',TaskValidation,TaskController.update);
 
 
 module.exports = router;
