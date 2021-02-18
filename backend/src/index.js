1226f9cf5b04b428a819b188bec05c2e66021081
Thiago Silva Costa <thiagosilvac01@gmail.com>
2021-02-18 17:18:31 -0300

    alterando porta para resolver conflito entre porta do backend e frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,6 @@ const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
 
 
-server.listen(3000, ()=>{
-    console.log('Executando na porta 3000');
+server.listen(3003, ()=>{
+    console.log('Executando na porta 3003');
 })
